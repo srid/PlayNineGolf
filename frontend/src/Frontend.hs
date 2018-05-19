1fe472cf16db9bcc32592887ff36aa7128221c38
Sridhar Ratnakumar <srid@srid.ca>
2018-05-19 12:10:27 -0400

    placeholder code

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -38,10 +38,48 @@ frontend = mainWidgetWithHead' (const head', const body)
     body = do
       divClass "ui container" $ divClass "ui segment" $ do
         elClass "h1" "ui header" $ text "Play Nine Golf"
+        controlWidget
         showProbabilities deck
+
+controlWidget :: MonadWidget t m => m ()
+controlWidget = divClass "ui raised segment" $ do
+  -- TODO: Use an increment (number) widget here
+  pile <- value <$> textInput def
+  -- TODO: Use a list widget here (to which we can append cards)
+  turned <- value <$> textInput def
+  let probabilities = zipDynWith calcProbabilities
+          (parsePile <$> pile)
+          (parseCards <$> turned)
+  divClass "ui segment" $ do
+    divClass "ui header" $ text "Probabilities"
+    divClass "ui contenet" $ do
+      el "tt" $ dynText $ fmap (T.pack . show) probabilities
+  return ()
+  where
+    parsePile = read . T.unpack
+    parseCards _ = []
 
 showProbabilities :: MonadWidget t m => [Card] -> m ()
 showProbabilities cards = divClass "ui cards" $ do
-  forM_ (group $ sort cards) $ \grp -> divClass "card" $ divClass "content" $ do
+  forM_ (group $ sort cards) $ \grp -> divClass "card link" $ divClass "content" $ do
     divClass "header" $ text $ T.pack $ show $ head grp
     divClass "description" $ text $ T.pack $ show $ length grp
+
+-- | Calculate the probablity of a card appearing next in the pile.
+calcProbabilities
+  :: Int  -- Number of cards on pile (unturned)
+  -> [Card] -- Cards on the table that are turned
+  -> [(Card, Double)]
+calcProbabilities inPile turned = [] -- TODO
+
+-- | Pop one card out of the pipe
+popPile
+  :: Int  -- Number of cards on pile (unturned)
+  -> Int
+popPile n = n - 1
+
+turnCard
+  :: [Card]  -- ^ Currently turned cards
+  -> Card  -- ^ Card that was just turned
+  -> [Card]
+turnCard xs x = x : xs
